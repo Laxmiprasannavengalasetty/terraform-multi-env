4b8a6823d480ee4b81e9d3b72f37a7dd6f2555a8
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-22 15:31:43 +1000

    terraform-workspace

---
+++ workspaces/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "terraform" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = lookup(var.instance_type, terraform.workspace)     #terraform.workspace means which shows which workspace you are in
+  vpc_security_group_ids = ["sg-026ccdb1c65b84e63"] # it's id of allow-all secutiry group
+
+
+  tags = {
+    Name = "terraform-${terraform.workspace}"
+  }
+}
